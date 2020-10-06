a1267ef24a03e342d0226e2323e5cf0c59c473cf
Shahbaz Mughal <shahbaz.mughal@hostingshouse.com>
2020-10-06 14:50:11 +0500

    latest data update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/qrbuzz.github.io/precache-manifest.3c4786dbc2e7faf6263fa2509284e550.js"
+  "/qrbuzz.github.io/precache-manifest.613c478a73a6df0a77c6bae7c035771e.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ static/js/xlsx.dbe7c98c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmumara=this.webpackJsonpmumara||[]).push([[2],{1397:function(n,a){},1421:function(n,a){},1422:function(n,a){}}]);
+//# sourceMappingURL=xlsx.dbe7c98c.chunk.js.map
